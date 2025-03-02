cf0b68d85d5f5069c41de851a20bf94add6f28c5
Juliya Lipatova <575@ukr.net>
2025-03-02 14:26:28 +0100

    load more button was added

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # image-finder-in
 The finder for images
+
+It was created with https://pixabay.com/  API
--- src/index.js
+++ src/index.js
@@ -8,10 +8,12 @@ import { cardImgTemplate } from './js/cardImgTemplate';
 const { formEl, galleryEl } = getRefs()
 
 const apiService = new ApiService()
-const loadMore = new LoadMoreBtnClass({ selector: '[data-set="load"]', hidden: true })
+const loadMoreEl = new LoadMoreBtnClass({ selector:'[data-action="Loader more"]', hidden: true })
+
+console.log(loadMoreEl)
 
 formEl.addEventListener("submit", onSearchImages)
-
+loadMoreEl.refs.button.addEventListener("click", onLoadMore)
 
 async function onSearchImages(e) {
     e.preventDefault()
@@ -35,12 +37,33 @@ async function onSearchImages(e) {
 
     const cardsMarkup = makeImagesMarkup(hits);
     renderCards(cardsMarkup)
-    loadMore.show()
+    loadMoreEl.show()
+  
  }catch(error){
     console.log(error)
  }finally{
     formEl.reset()
  }
+}
+
+async function onLoadMore(e) {
+    e.preventDefault();
+    loadMoreEl.disable();  
+
+    try {
+        const result = await apiService.fetchArticles(); 
+        console.log(result);
+
+        const { hits, totalHits } = result;
+        apiService.incrementPage();  
+
+        const cardsMarkup = makeImagesMarkup(hits);  
+        renderCards(cardsMarkup);  
+    } catch (error) {
+        console.log(error);
+    } finally {
+        loadMoreEl.enable();  
+    }
 }
 
 function makeImagesMarkup(images) {
